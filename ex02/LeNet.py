6023ed5b3afe2698f1c43b097992c4748c6bf9bb
炜轩 梁 <liangwx23@mail2.sysu.edu.cn>
2022-05-15 00:56:13 +0800

    Finish machine learning hw2

--- ex02/LeNet.py
+++ ex02/LeNet.py
@@ -4,40 +4,18 @@ import torchvision
 import torch.nn.functional as F
 import torchvision.transforms as transforms
 from   torch.autograd import Variable
+import matplotlib
+from matplotlib import pyplot as plt 
 
-help(nn.Conv2d)
-
-'''定义参数'''
-batch_size = 64
-lr = 0.001
-num_classes = 10
-
-'''获取数据集'''
-train_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.MNIST('mnist_data', train=True, download=True,
-                               transform=torchvision.transforms.Compose([
-                                   torchvision.transforms.ToTensor(),
-                                   torchvision.transforms.Normalize(
-                                       (0.1307,), (0.3081,))
-                               ])),
-    batch_size=batch_size, shuffle=True)
-
-test_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.MNIST('mnist_data/', train=False, download=True,
-                               transform=torchvision.transforms.Compose([
-                                   torchvision.transforms.ToTensor(),
-                                   torchvision.transforms.Normalize(
-                                       (0.1307,), (0.3081,))
-                               ])),
-    batch_size=batch_size, shuffle=False)
-
-
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]  =  "TRUE"
+plt.rc('font',family='Times New Roman')
 '''设计LeNet_5'''
 class LeNet(nn.Module):
     def __init__(self, num_classes=10):
         super(LeNet, self).__init__()
         '''第一层卷积，卷积核大小为5*5，步距为1，输入通道为3，输出通道为6'''
-        self.conv1 = nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=2)
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=1)
 
         '''第一层池化层，卷积核为2*2，步距为2，相当于特征图缩小了一半'''
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -49,12 +27,12 @@ class LeNet(nn.Module):
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         '''第一层全连接层，维度由16*5*5=>120'''
-        self.linear1 = nn.Linear(16 * 5 * 5, 120)
+        self.linear1 = nn.Linear(16 * 4 * 4, 120)
 
         '''第二层全连接层，维度由120=>84'''
         self.linear2 = nn.Linear(120, 84)
 
-        '''第三层全连接层，维度由64=>10'''
+        '''第三层全连接层，维度由84=>10'''
         self.linear3 = nn.Linear(84, num_classes)
 
     def forward(self, x):
@@ -71,7 +49,7 @@ class LeNet(nn.Module):
         out = self.pool2(out)
 
         '''将池化层后的数据进行Flatten，使数据变成能够被FC层接受的Vector'''
-        out = out.view(-1, 16 * 5 * 5)
+        out = out.view(-1, 16 * 4 * 4)
 
         '''将数据送入第一个全连接层'''
         out = torch.sigmoid(self.linear1(out))
@@ -84,45 +62,86 @@ class LeNet(nn.Module):
 
         return out
 
+def main():
 
-model = LeNet(num_classes)
+    def plot(losses):
+        fig = plt.figure(figsize=(9, 6))
+        ax = fig.add_subplot(111)
 
-'''设置损失函数'''
-criterion = nn.CrossEntropyLoss()
+        ax.plot(range(1,len(losses[0]) + 1), losses[0], label = "5 iters")
+        ax.plot(range(1,len(losses[1]) + 1), losses[1], label = "10 iters")
 
-'''设置优化器'''
-optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+        ax.set_xlabel("iter times",fontsize = 10)
+        ax.set_ylabel('loss',fontsize = 10)
 
-'''开始训练'''
-total_step = len(train_loader)
-for epoch in range(1):
+        plt.legend()
+        plt.savefig(r"C:\Users\lwx\Dropbox\SWOT_simulator\LearnAndTest\ex02\test.png", dpi=300, pad_inches = 0)
+
+    '''定义参数'''
+    batch_size = 64
+    lr = 0.001
+    num_classes = 10
+
+    '''获取数据集'''
+    train_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.MNIST('mnist_data', train=True, download=True,
+                                   transform=torchvision.transforms.Compose([
+                                       torchvision.transforms.ToTensor(),
+                                       torchvision.transforms.Normalize(
+                                           (0.1307,), (0.3081,))
+                                   ])),
+        batch_size=batch_size, shuffle=True)
+
+    test_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.MNIST('mnist_data/', train=False, download=True,
+                                   transform=torchvision.transforms.Compose([
+                                       torchvision.transforms.ToTensor(),
+                                       torchvision.transforms.Normalize(
+                                           (0.1307,), (0.3081,))
+                                   ])),
+        batch_size=batch_size, shuffle=False)
+
+    losses = []
+    for iterTime in [5,10]:
+        model = LeNet(num_classes)
+        losss = []
+        criterion = nn.CrossEntropyLoss()
+
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+        total_step = len(train_loader)
+        for epoch in range(iterTime):
+            for i, (images, labels) in enumerate(train_loader):
+
+                images = Variable(images)
+                labels = Variable(labels)
+                # Forward pass
+                outputs = model(images)
+                loss = criterion(outputs, labels)
+
+                # Backward and optimize
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                if (i + 1) % 100 == 0:
+                    print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                          .format(epoch + 1, 10, i + 1, total_step, loss.item()))
+                losss.append(loss.item())
+        losses.append(losss)
+    plot(losses)
+    correct = 0  # 预测正确的图片数
+    total = 0  # 总共的图片数
     for i, (images, labels) in enumerate(train_loader):
-
         images = Variable(images)
         labels = Variable(labels)
         # Forward pass
         outputs = model(images)
-        loss = criterion(outputs, labels)
+        _, predicted = torch.max(outputs, 1)
 
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        total += labels.size(0)
+        correct += (predicted == labels).sum()
 
-        if (i + 1) % 100 == 0:
-            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                  .format(epoch + 1, 10, i + 1, total_step, loss.item()))
+    print('准确率为: %d %%' % (100 * correct / total))
 
-correct = 0  # 预测正确的图片数
-total = 0  # 总共的图片数
-for i, (images, labels) in enumerate(train_loader):
-    images = Variable(images)
-    labels = Variable(labels)
-    # Forward pass
-    outputs = model(images)
-    _, predicted = torch.max(outputs, 1)
-
-    total += labels.size(0)
-    correct += (predicted == labels).sum()
-
-print('准确率为: %d %%' % (100 * correct / total))
+if __name__ == '__main__':
+    main()
--- ex02/MLHomeWork01.py
+++ ex02/MLHomeWork01.py
@@ -1,45 +1,88 @@
 import pandas as pd
+import numpy  as np
+
 from sklearn.neural_network  import MLPClassifier
 from sklearn.model_selection import train_test_split
-import numpy as np
-
-train = pd.read_table(r"C:\Users\lwx\Dropbox\SWOT_simulator\LearnAndTest\ex02\train.txt", 
-                      sep = " ",
-                      header = None)
-
-test = pd.read_table(r"C:\Users\lwx\Dropbox\SWOT_simulator\LearnAndTest\ex02\test.txt", 
-                      sep = " ",
-                      header = None)
+from sklearn.ensemble        import RandomForestClassifier 
+from sklearn.svm             import SVC
 
 def cal_accuracy(pre, tru, name):
+
     pre = np.asarray(pre)
     tru = np.asarray(tru)
     acc = (len(pre) - (np.abs(pre - tru)).sum()) * 100 / len(pre)
     info = name + " accuracy: " + str(acc) + "%"
+
     return acc, info
 
+def BPtraining(clf, name, X_train, y_train, X_test, y_test):
 
-train, test = train_test_split(train, test_size = 0.3)
-X_train = train[[1,2,3,4]]
-y_train = train[[0]]
-X_test = test[[1,2,3,4]]
-y_test = test[[0]]
-def training(clf,name):
-    clf.fit(X_train,y_train)
+    clf.fit(X_train, y_train)
     pre = clf.predict(X_test)
     tru = y_test[0]
-    acc, info = cal_accuracy(pre,tru, name)
+    acc, info = cal_accuracy(pre, tru, name)
     return info
-size = [50, 100, 150]
-layers = [2, 4, 6]
-infos = []
-for i in size:
-    for j in layers:
-        hidden_layer_sizes = (i,) * j
-        clf = MLPClassifier(solver='adam', alpha=1e-5,
-                     hidden_layer_sizes=hidden_layer_sizes, random_state=1)
-        name = str(hidden_layer_sizes)
-        info = training(clf, name)
-        infos.append(info)
+
+def RFtraining(X_train, y_train, X_test, y_test):
+
+    rf = RandomForestClassifier()           
+    rf.fit(X_train, y_train)
+    pre = rf.predict(X_test)
+    tru = y_test[0]
+    acc, info = cal_accuracy(pre, tru, "RF")
+    return info
+
+def BPtrainingu(X_train, y_train, X_test, y_test):
+
+    clf = MLPClassifier()           
+    clf.fit(X_train, y_train)
+    pre = clf.predict(X_test)
+    tru = y_test[0]
+    acc, info = cal_accuracy(pre, tru, "BP")
+    return info
+
+def SVMtraining(X_train, y_train, X_test, y_test):
+    svc = SVC()           
+    svc.fit(X_train, y_train)
+    pre = svc.predict(X_test)
+    tru = y_test[0]
+    acc, info = cal_accuracy(pre, tru, "SVC")
+    return info
+
+def main():
+
+    train = pd.read_table(r"C:\Users\lwx\Dropbox\SWOT_simulator\LearnAndTest\ex02\train.txt", 
+                          sep = " ",
+                          header = None)
+
+    test = pd.read_table(r"C:\Users\lwx\Dropbox\SWOT_simulator\LearnAndTest\ex02\test.txt", 
+                          sep = " ",
+                          header = None)
+
+    train, test = train_test_split(train, test_size = 0.3)
+
+    X_train = train[[1,2,3,4]]
+    y_train = train[[0]]
+    X_test = test[[1,2,3,4]]
+    y_test = test[[0]]
+
+    size = [50, 100, 150]
+    layers = [2, 4, 6]
+    infos = []
+
+    for i in size:
+        for j in layers:
+            hidden_layer_sizes = (i,) * j
+            clf = MLPClassifier(solver='adam', alpha=1e-5,
+                            hidden_layer_sizes=hidden_layer_sizes, random_state=1)
+            name = str(hidden_layer_sizes)
+            info = BPtraining(clf, name, X_train, y_train, X_test, y_test)
+            infos.append(info)
         
-print(infos)
+    infos.append(SVMtraining(X_train, y_train, X_test, y_test))
+    infos.append(BPtrainingu(X_train, y_train, X_test, y_test))
+    infos.append(RFtraining(X_train, y_train, X_test, y_test))
+    print(infos)
+
+if __name__ == '__main__':
+    main()
